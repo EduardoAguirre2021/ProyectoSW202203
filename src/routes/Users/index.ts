147f98dcf48642b560a7873701918c9d439ad6dd
Luigi594 <luishnlacho@gmail.com>
2022-12-01 20:58:46 -0600

    Some changes in login

--- src/routes/Users/index.ts
+++ src/routes/Users/index.ts
@@ -1,194 +1,221 @@
-import  express  from "express";
-import { body, validationResult } from "express-validator";
-import { Users } from "@libs/Users/index";
-const router= express.Router();
-const users= new Users();
+import express from 'express';
+import { body, validationResult } from 'express-validator';
+import { Users } from '@libs/Users/index';
+const router = express.Router();
+const users = new Users();
 
-
-router.post('/signin', 
-body('email').isEmail().withMessage("Email enviado tiene formato incorrecto") ,
-body('password').isStrongPassword({minLength:8}).withMessage("Contraseña no cumple con los requisitos"),
-body('username').isLength({min:5}).withMessage("El username no cumple con la longitud minima"),
-body('birthDate').isDate().withMessage("Formato incorrecto"),
-async (req, res) => {
-
+router.post(
+  '/signin',
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  body('password')
+    .isStrongPassword({ minLength: 8 })
+    .withMessage('Contraseña no cumple con los requisitos'),
+  body('username')
+    .isLength({ min: 5 })
+    .withMessage('El username no cumple con la longitud minima'),
+  body('birthDate').isDate().withMessage('Formato incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
-        } else {
-            const {name, username, email, birthDate,password} = req.body;     
-            const result= await users.signin(name, username, email, password, birthDate);
-            console.log("Usuario Creado: ", result);
-            res.status(200).json({result: true, msg: "Usuario Creado exitosamente"});
-        }
-
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { name, username, email, birthDate, password } = req.body;
+        const result = await users.signin(
+          name,
+          username,
+          email,
+          password,
+          birthDate,
+        );
+        console.log('Usuario Creado: ', result);
+        res
+          .status(200)
+          .json({ result: true, msg: 'Usuario Creado exitosamente' });
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({result: false, error: "Error al crear el usuario"});
+      console.log('Error: ', error);
+      res
+        .status(500)
+        .json({ result: false, error: 'Error al crear el usuario' });
     }
+  },
+);
 
-
-});
-
-router.put('/update/:id',
-body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"),
-body('username').isLength({min:5}).withMessage("El username no cumple con la longitud minima"),
-body('status').isIn(['ACT', 'INA']),
-async (req,res)=> {
+router.put(
+  '/update/:id',
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  body('username')
+    .isLength({ min: 5 })
+    .withMessage('El username no cumple con la longitud minima'),
+  body('status').isIn(['ACT', 'INA']),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
-        } else {
-            const {id}= req.params;
-            const {username,email,status}= req.body;
-            const result= await users.updateUser(id, username, email, status);
-            console.log("Usuario actualizado: ", result);
-            res.status(200).json({result: true, msg: "Usuario Actualizado exitosamente"});
-        }
-    
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { id } = req.params;
+        const { username, email, status } = req.body;
+        const result = await users.updateUser(id, username, email, status);
+        console.log('Usuario actualizado: ', result);
+        res
+          .status(200)
+          .json({ result: true, msg: 'Usuario Actualizado exitosamente' });
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al actualizar el usuario"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al actualizar el usuario' });
     }
-});
+  },
+);
 
 router.get('/getUsers', async (_req, res) => {
+  try {
+    const result = await users.getAll();
+    console.log(result);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log('Error: ', error);
+    res.status(500).json({ error: 'Error al cargar los datos' });
+  }
+});
+
+router.get(
+  '/byemail',
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  async (req, res) => {
     try {
-        const result= await users.getAll();
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { email } = req.body;
+        const result = await users.findUserByEmail(email);
         console.log(result);
         res.status(200).send(result);
-        } catch (error) {
-            console.log("Error: ", error);
-            res.status(500).json({error: "Error al cargar los datos"});
-    }
-});
-
-router.get('/byemail',body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"),
- async (req, res) => {
-    try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
-        }
-        else {
-            const {email}= req.body; 
-            const result= await users.findUserByEmail(email);
-            console.log(result);
-            res.status(200).send(result);
-           
-        }
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-});
+  },
+);
 
 router.get('/byusername', async (req, res) => {
-    try {
-            const {username}= req.body;
-            const result= await users.findUserByUsername(username);
-            console.log(result);
-            res.status(200).send(result);
-    } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
-    }
+  try {
+    const { username } = req.body;
+    const result = await users.findUserByUsername(username);
+    console.log(result);
+    res.status(200).send(result);
+  } catch (error) {
+    console.log('Error: ', error);
+    res.status(500).json({ error: 'Error al cargar los datos' });
+  }
 });
 
-router.put('/addrole/:id', body('role').isIn(['public', 'admin', 'auditor', 'support']).withMessage("Role incorrecto") ,
-async (req, res) => {
+router.put(
+  '/addrole/:id',
+  body('role')
+    .isIn(['public', 'admin', 'auditor', 'support'])
+    .withMessage('Role incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
-        }
-        else {
-            const {id}= req.params;
-            const {role}= req.body;
-            const result= await users.addUserRole(id, role);
-            console.log("Rol añadido", result);
-            res.status(200).json({msg: "Rol añadido", result: true});
-        }
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { id } = req.params;
+        const { role } = req.body;
+        const result = await users.addUserRole(id, role);
+        console.log('Rol añadido', result);
+        res.status(200).json({ msg: 'Rol añadido', result: true });
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-});
+  },
+);
 
-
-router.put('/removerole/:id', body('role').isIn(['public', 'admin', 'auditor', 'support']).withMessage("Role incorrecto") ,
-async (req, res) => {
+router.put(
+  '/removerole/:id',
+  body('role')
+    .isIn(['public', 'admin', 'auditor', 'support'])
+    .withMessage('Role incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
-        }
-        else {
-            const {id}= req.params;
-            const {role}= req.body;
-            const result= await users.removeRoleToUser(id, role);
-            console.log("Rol eliminado", result);
-            res.status(200).json({msg: "Rol eliminado", result: true});
-        }
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { id } = req.params;
+        const { role } = req.body;
+        const result = await users.removeRoleToUser(id, role);
+        console.log('Rol eliminado', result);
+        res.status(200).json({ msg: 'Rol eliminado', result: true });
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-})
+  },
+);
 
 //Agregar ruta para login
 
-router.get('/login', body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"), 
-async (req, res) => {
+router.post(
+  '/login',
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { email, password } = req.body;
+        const result = await users.login(email, password);
+        if (result == true) {
+          res.status(200).json(result);
+        } else {
+          res.status(200).json({ msg: 'Login fallido', result: false });
         }
-        else {
-            const {email, password}= req.body;
-            const result= await users.login(email, password);
-            if(result == true)
-            {
-                res.status(200).json({msg: "Login exitoso", result: true});
-            }
-            else
-            {
-                res.status(200).json({msg: "Login fallido", result: false});
-            }
-        }
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-});
+  },
+);
 
-router.get('/recoverpassword', body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"),
-async (req, res) => {
+router.get(
+  '/recoverpassword',
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { email } = req.body;
+        const result = await users.recoverPassword(email);
+        if (result == true) {
+          res
+            .status(200)
+            .json({ msg: 'Correo Enviado Exitosamente!', result: true });
+        } else {
+          res.status(200).json({
+            msg: 'Correo no enviado, verifique la direccion de Correo Electronico',
+            result: false,
+          });
         }
-        else {
-            const {email}= req.body;
-            const result= await users.recoverPassword(email);
-            if(result == true)
-            {
-                res.status(200).json({msg: "Correo Enviado Exitosamente!", result: true});
-            }
-            else
-            {
-                res.status(200).json({msg: "Correo no enviado, verifique la direccion de Correo Electronico", result: false});
-            }
-        }
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-});
+  },
+);
 
 /*router.get('/verifypin', body('pin').isNumeric().withMessage("Pin enviado tiene formato incorrecto"),
 body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"),
@@ -217,41 +244,45 @@ async (req, res) => {
 });
 */
 
-router.get('/changepassword', body('password').isLength({min: 7}).withMessage("Password enviado tiene formato incorrecto"),
-body('confirmpassword').isLength({min: 7}).withMessage("Confirm Password enviado tiene formato incorrecto"),
-body('email').isEmail().withMessage("Email enviado tiene formato incorrecto"),
-async (req, res) => {
+router.get(
+  '/changepassword',
+  body('password')
+    .isLength({ min: 7 })
+    .withMessage('Password enviado tiene formato incorrecto'),
+  body('confirmpassword')
+    .isLength({ min: 7 })
+    .withMessage('Confirm Password enviado tiene formato incorrecto'),
+  body('email').isEmail().withMessage('Email enviado tiene formato incorrecto'),
+  async (req, res) => {
     try {
-        const validation= validationResult(req);
-        if(!validation.isEmpty()) {
-            res.status(400).json({errors: validation.array() });
+      const validation = validationResult(req);
+      if (!validation.isEmpty()) {
+        res.status(400).json({ errors: validation.array() });
+      } else {
+        const { password, confirmpassword, email } = req.body;
+        //const result= await users.changePassword(password, confirmPassword, email);
+        if (password != confirmpassword) {
+          res
+            .status(200)
+            .json({ msg: 'Las contraseñas no coinciden', result: false });
+        } else {
+          const result = await users.changePassword(email, password);
+          if (result == true) {
+            res
+              .status(200)
+              .json({ msg: 'Contraseña cambiada exitosamente', result: true });
+          } else {
+            res
+              .status(200)
+              .json({ msg: 'No se pudo cambiar la contraseña', result: false });
+          }
         }
-        else {
-            const {password, confirmpassword, email}= req.body;
-            //const result= await users.changePassword(password, confirmPassword, email);
-            if(password != confirmpassword)
-            {
-                res.status(200).json({msg: "Las contraseñas no coinciden", result: false});
-            }
-            else
-            {   
-                const result = await users.changePassword(email, password);
-                if(result == true)
-                {
-                    res.status(200).json({msg: "Contraseña cambiada exitosamente", result: true});
-                }
-                else
-                {
-                    res.status(200).json({msg: "No se pudo cambiar la contraseña", result: false});
-                }
-            }
-        }
+      }
     } catch (error) {
-        console.log("Error: ", error);
-        res.status(500).json({error: "Error al cargar los datos"});
+      console.log('Error: ', error);
+      res.status(500).json({ error: 'Error al cargar los datos' });
     }
-});
-
-
+  },
+);
 
 export default router;
